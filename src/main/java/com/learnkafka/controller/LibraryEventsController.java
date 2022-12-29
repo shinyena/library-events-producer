11cbe7c620afa72f5b5f8e8f40ef64f3ffea08ab
yenashin <yena5790@gachon.ac.kr>
2022-12-30 00:48:48 +0900

    add produce kafka topic & log

--- src/main/java/com/learnkafka/controller/LibraryEventsController.java
+++ src/main/java/com/learnkafka/controller/LibraryEventsController.java
@@ -1,6 +1,10 @@
 package com.learnkafka.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.learnkafka.domain.LibraryEvent;
+import com.learnkafka.producer.LibraryEventProducer;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,10 +12,18 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class LibraryEventsController {
+
+    @Autowired
+    LibraryEventProducer libraryEventProducer;
+
     @PostMapping("/v1/libraryevent")
-    public ResponseEntity<LibraryEvent> postLibraryEvent(@RequestBody LibraryEvent libraryEvent) {
+    public ResponseEntity<LibraryEvent> postLibraryEvent(@RequestBody LibraryEvent libraryEvent) throws JsonProcessingException {
         // invoke kafka producer
+        log.info("before sendLibraryEvent");
+        libraryEventProducer.sendLibraryEvent(libraryEvent);
+        log.info("after sendLibraryEvent");
         return ResponseEntity.status(HttpStatus.CREATED).body(libraryEvent);
     }
 }
